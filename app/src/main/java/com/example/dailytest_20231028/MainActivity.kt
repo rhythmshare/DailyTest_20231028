8999f19846fbf3382574efcd98f5f8af16231eba
rhythmshare <randomcony@gmail.com>
2023-10-28 17:47:14 +0900

    Login success/failure based on conditions

--- app/src/main/java/com/example/dailytest_20231028/MainActivity.kt
+++ app/src/main/java/com/example/dailytest_20231028/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.dailytest_20231028.databinding.ActivityMainBinding
+import kotlin.io.path.Path
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -16,6 +17,13 @@ class MainActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
             val inputEmail = binding.edtEmail.text.toString()
             val inputPwd = binding.edtPassword.text.toString()
+
+            if( inputEmail == "admin@test.com" && inputPwd == "qwer"){
+                Toast.makeText(this,"관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"로그인 실패입니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
